78988dc772f8c57574755dc2f26b5236c10a05fc
David Hayes <dave@pagerduty.com>
2011-11-03 23:30:55 -0700

    Added meta-error handling

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from google.appengine.ext.webapp import util
 import random
 
 class MainHandler(webapp.RequestHandler):
+  errors = [400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,500,501,502,503,504,505]
   def get(self):
     opts = self.request.path.split('/')
 
@@ -12,9 +13,11 @@ class MainHandler(webapp.RequestHandler):
       odds = 1
 
     try:
-      err = int(opts[2])
+      err = 500;
+      if(int(opts[2]) in self.errors):
+        err = int(opts[2])
     except:
-      err = random.choice([400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,500,501,502,503,504,505])
+      err = random.choice(errors)
 
     if( random.randint(1, odds) == 1 ):
       self.response.set_status(err)
